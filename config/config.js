26fd820679b6111437dc6f7f6fe4452b7a5b41a5
Shikhar03Stark <hv.harshit321@gmail.com>
2021-07-12 21:19:27 +0530

    Load env vars

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: 'runiv',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
